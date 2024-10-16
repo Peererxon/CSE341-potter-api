e14ccc13443d147d4ab8623afb5699a0a00d104c
andersongil <andersongil.dev@gmail.com>
2024-10-16 12:57:38 -0400

    removing duplicates routes calling

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,15 @@ const router = express.Router();
 const characterRoutes = require('./characters');
 const movieRoutes = require('./movies');
 const spellRoutes = require('./spells');
-
-router.use('/characters', require('./characters'));
-router.use('/movies', require('./movies'));
-router.use('/spells', require('./spells'));
+//const houseRoutes = require('./houses');
 
 router.use('/characters', characterRoutes);
 router.use('/movies', movieRoutes);
 router.use('/spells', spellRoutes);
-
+//router.use('/houses', houseRoutes);
 
 router.get('/test', (_, res) => {
-    res.send('API is working!');
+	res.send('API is working!');
 });
 
 module.exports = router;
